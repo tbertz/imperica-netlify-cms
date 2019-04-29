215acb880f2768ebc7f33acdd4071e83c06db081
Tom Burton <burtot07@mc-s104490.local>
2019-04-29 10:27:51 +0100

    all the pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,11 +1,19 @@
 import CMS from 'netlify-cms'
 
+import MagazinePagePreview from './preview-templates/MagazinePagePreview'
+import NewsletterPagePreview from './preview-templates/NewsletterPagePreview'
+import ServicesPagePreview from './preview-templates/ServicesPagePreview'
+import MediaPagePreview from './preview-templates/MediaPagePreview'
 import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
 
 CMS.registerPreviewTemplate('index', IndexPagePreview)
+CMS.registerPreviewTemplate('magazine', MagazinePagePreview)
+CMS.registerPreviewTemplate('newsletter', NewsletterPagePreview)
+CMS.registerPreviewTemplate('services', ServicesPagePreview)
+CMS.registerPreviewTemplate('media', MediaPagePreview)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
